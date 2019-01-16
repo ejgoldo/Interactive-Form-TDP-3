79a32434a89ee7b3e35f3cf02cc57aaa33765cc7
EJ Serafin <edgardo@chainblx.com>
2019-01-15 20:42:14 -0800

    display colors to themes
    got this working but with bugs. need to work on this further

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ otherTitle.style.display = 'none';
 
 const jobRoles = document.getElementById('title');
 jobRoles.addEventListener('change', (event) => {
-    if (jobRoles.value === 'other') {
+    if (event.target.value === 'other') {
         otherTitle.style.display = '';
     } else {
         otherTitle.style.display = 'none';
@@ -20,24 +20,37 @@ jobRoles.addEventListener('change', (event) => {
 const shirtDesign = document.getElementById('design');
 const shirtColor = document.getElementById('color');
 const allColors = document.getElementById('colors-js-puns');
-const jsPuns = document.querySelectorAll('#color option:nth-child(-n+3)');
-const heartJs = document.querySelectorAll('#color option:nth-child(n+4)');
 
 // hide shirt colors and Color: label if no theme is selected
 allColors.style.display = 'none';
 
 // show only appropriate colors if one of the two theme options is selected. if not keep in display: none
 shirtDesign.addEventListener('change', (event) => {
-    if (shirtDesign.value === 'js puns') {
+    if (event.target.value === 'js puns') {
         allColors.style.display = '';
-        allColors.removeChild(heartJs[i]);
-    } else if (shirtDesign.value === 'heart js') {
+        for (let i=0; i<shirtColor.length; i++) {
+            let chosenColors = shirtColor.options[i].value;
+            if (chosenColors === 'tomato' || chosenColors === 'steelblue' || chosenColors === 'dimgrey') {
+                shirtColor.options[i].remove();
+                i--;
+            }
+        }
+    } else if (event.target.value === 'heart js') {
         allColors.style.display = '';
-        allColors.removeChild(jsPuns[i]);
+        for (let i=0; i<shirtColor.length; i++) {
+            let chosenColors = shirtColor.options[i].value;
+            if (chosenColors === 'cornflowerblue' || chosenColors === 'darkslategrey' || chosenColors === 'gold') {
+                shirtColor.options[i].remove();
+                i--;
+            }
+        }
     } else {
         allColors.style.display = 'none';
     }
 });
 
 // so far, can hide the colors completely and when neither of the two themes are selected, its hidden again.
-// TO FIX: only displaying the colors necessary when one theme is selected and hiding the rest
+// also, able to display colors to each theme.
+
+// TO FIX: currently when page loads, depending on which theme is selected, the respective colors show. however, after a theme is chosen, 
+//         when you select another theme, no colors show. they are blank. 
